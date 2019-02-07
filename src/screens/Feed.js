cbac16c9177b35f1584f701ea082d5cb95ac89bc
Andric Tham <andric@nuisance.co>
2019-02-07 17:03:33 +0800

    Scaffold mock photo feed

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 
 class Feed extends Component {
-	constructor(props) {
-		super(props);
-	}
+	state = {
+		photos: [0, 1, 2, 3, 4],
+		isRefreshing: false,
+	};
 
+	loadNewPhotos = () => {
+		this.setState({
+			isRefreshing: true,
+		});
+		this.setState({
+			photos: [5, 6, 7, 8, 9],
+			isRefreshing: false,
+		});
+	};
 	render() {
 		return (
 			<View style={{ flex: 1 }}>
@@ -22,26 +32,36 @@ class Feed extends Component {
 				>
 					<Text>Feed</Text>
 				</View>
-				<View>
-					<View>
-						<Text>Time Ago</Text>
-						<Text>@rusty</Text>
-					</View>
-					<View>
-						<Image
-							source={{
-								uri:
-									"https://source.unsplash.com/random/500x" +
-									Math.floor(Math.random() * 800 + 500),
-							}}
-							style={{ resizeMode: "cover", width: "100%", height: 275 }}
-						/>
-					</View>
-					<View>
-						<Text>Caption text here...</Text>
-						<Text>View comments</Text>
-					</View>
-				</View>
+
+				<FlatList
+					refreshing={this.state.isRefreshing}
+					onRefresh={this.loadNewPhotos}
+					data={this.state.photos}
+					keyExtractor={(item, index) => index.toString()}
+					style={{ flex: 1, backgroundColor: "#fff" }}
+					renderItem={(item, index) => (
+						<View key={index} style={{ marginBottom: 18 }}>
+							<View>
+								<Text>Time Ago</Text>
+								<Text>@rusty</Text>
+							</View>
+							<View>
+								<Image
+									source={{
+										uri:
+											"https://source.unsplash.com/random/500x" +
+											Math.floor(Math.random() * 800 + 500),
+									}}
+									style={{ resizeMode: "cover", width: "100%", height: 275 }}
+								/>
+							</View>
+							<View>
+								<Text>Caption text here...</Text>
+								<Text>View comments</Text>
+							</View>
+						</View>
+					)}
+				/>
 			</View>
 		);
 	}
